5ca33e0c092367876433b27d4b3978060956fca1
1988warren <1988warren@gmail.com>
2019-06-07 15:23:55 -0400

    spec branch

--- core/src/main/java/edu/gwu/cs6431/multichat/core/protocol/client/MessageType.java
+++
@@ -1,5 +0,0 @@
-package edu.gwu.cs6431.multichat.core.protocol.client;
-
-public enum MessageType {
-    CHAT, QUERY, FETCH, NICKNAME, BYE
-}
--- core/src/main/java/edu/gwu/cs6431/multichat/core/protocol/server/ResponseStatus.java
+++
@@ -1,5 +0,0 @@
-package edu.gwu.cs6431.multichat.core.protocol.server;
-
-public enum ResponseStatus {
-    OK, ERROR
-}
